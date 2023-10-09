704fda406bf5d99d9ecc4fcee5e0c0c669e16bb2
Thilo Billerbeck <thilo.billerbeck@officerent.de>
2023-10-09 17:52:06 +0200

    started implementing figma mockup of base ui

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,24 @@ module.exports = {
     "./templates/**/*.html.twig",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'dp-orange': {
+          DEFAULT: '#F6590C',
+          50: '#FEF0E9',
+          100: '#FDDFD1',
+          200: '#FBBEA0',
+          300: '#FA9C6E',
+          400: '#F87B3D',
+          500: '#F6590C',
+          600: '#CD4808',
+          700: '#A03906',
+          800: '#742904',
+          900: '#481903',
+          950: '#321202'
+        },
+      }
+    },
   },
   plugins: [],
 }
