6a79113664950498e631cd69387265810d613f25
Thilo Billerbeck <thilo.billerbeck@officerent.de>
2023-11-15 19:42:55 +0100

    ported over macros to symfony forms

--- src/Controller/TestController.php
+++ src/Controller/TestController.php
@@ -18,14 +18,14 @@ use Symfony\Component\Routing\Annotation\Route;
  */
 class TestController extends AbstractController
 {
-    #[Route('/dev/leaflet', name: 'leaflet')]
+    #[Route('/dev/leaflet', name: 'dev_leaflet', condition: '%kernel.debug% === 1')]
     public function leaflet(): Response
     {
         return $this->render('dev/leaflet.html.twig', [
             'controller_name' => 'TailwindController',
         ]);
     }
-    #[Route('/dev/ui', name: 'leaflet')]
+    #[Route('/dev/ui', name: 'dev_ui', condition: '%kernel.debug% === 1')]
     public function ui(): Response
     {
         return $this->render('dev/ui.html.twig', [
@@ -33,21 +33,34 @@ class TestController extends AbstractController
         ]);
     }
 
-    #[Route('/dev/form', name: 'form')]
+    #[Route('/dev/form', name: 'dev_form', condition: '%kernel.debug% === 1')]
     public function form(): Response
     {
+        $choiceOptions = [
+            'Choice 1' => "1",
+            'Choice 2' => "2",
+            'Choice 3' => "3",
+        ];
+
         $form = $this->createFormBuilder()
-            ->add('text', TextType::class, ['help' => 'Short Help Text'])
+            ->add('text', TextType::class, [
+                'help' => 'Short Help Text'
+            ])
             ->add('textarea', TextareaType::class)
             ->add('number', NumberType::class)
             ->add('date', DateType::class)
             ->add('range', RangeType::class)
-            ->add('choice', ChoiceType::class, [
-                'choices'  => [
-                    '1' => "1",
-                    '2' => "2",
-                    '3' => "3",
-                ],
+            ->add('choice_dropdown', ChoiceType::class, [
+                'choices'  => $choiceOptions
+            ])
+            ->add('choice_select', ChoiceType::class, [
+                'choices'  => $choiceOptions,
+                'expanded' => true,
+                'multiple' => true
+            ])
+            ->add('choice_radio', ChoiceType::class, [
+                'choices'  => $choiceOptions,
+                'expanded' => true
             ])
             ->add('save', SubmitType::class, ['label' => 'Submit'])
             ->getForm();
